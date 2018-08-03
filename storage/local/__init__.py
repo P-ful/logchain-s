940e197f9902e64a45fc3ed2dfb9f975c7087f67
Daeyeon Joo <daeyeonjoo@p-ful.com>
2018-08-03 12:47:20 +0900

    파라메터 교정

--- storage/local/__init__.py
+++ storage/local/__init__.py
@@ -1,23 +1,23 @@
 import os
 
 class LocalDisk:
-    def append(file_name, message):
+    def append(self, file_name, message):
         f = open(file_name, 'a')
         f.write(message)
         f.close()
 
-    def write(file_ame, message):
+    def write(self, file_name, message):
         f = open(file_name, 'w')
         f.write(message)
         f.close()
 
-    def read(file_name):
+    def read(self, file_name):
         f = open(block_storage_path + 'a_my_block', 'r')
         block = f.read()
         f.close()
         return block
 
-    def read_all_line(file_name):
+    def read_all_line(self, file_name):
         f = open(file_name, 'r')
         line_list = []
         while True:
